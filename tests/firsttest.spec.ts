36a3b83627c8212b9a634c0df3213604ad65a8a0
Cicd <project_507464_bot_25d72933a384105a6d98c719715f9478@noreply.gitlab.tech.orange>
2026-09-09 21:22:54 +0530

    playwright commit

---
+++ tests/firsttest.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://app.thetestingacademy.com/playwright/ttacart/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('vinay');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('123');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="error"]').click();
+});

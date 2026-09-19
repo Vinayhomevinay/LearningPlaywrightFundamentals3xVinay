2a52aad93fb181cc5d73cb837a04ab99d973364b
Vinay Kumar <vinayautomation2912@gmail.com>
2026-09-19 19:04:19 +0530

    test

--- tests/03_Locator_Commands/Practice.spec.ts
+++ tests/03_Locator_Commands/Practice.spec.ts
@@ -6,6 +6,13 @@ test("Test the xpath functions using wingify",async ({page})=>{
 
     await page.locator("//input[@class='WInput']").fill ("abcd");
 
+    await page.locator("#free-trial-step1-gdpr-consent-checkboxcu-marketing-consent-checkbox").click();
+    await page.locator("#free-trial-step1-gdpr-consent-checkboxcu-gdpr-consent-checkbox").check();
+    await page.locator("//span[@class='WButton-text']").first().click();
+    let error_message= await page.locator("//div[contains(@class,'invalid-reason')]").first().textContent();
+
+    expect(error_message).toContain("The email address you entered is incorrect.");
+
     await page.pause();
 
 

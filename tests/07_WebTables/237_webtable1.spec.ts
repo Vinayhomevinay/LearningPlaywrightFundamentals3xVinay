f7f64591130ef94aab459d75ebedfc2396e5166f
Vinay Kumar <vinayautomation2912@gmail.com>
2026-09-26 11:33:40 +0530

    test

--- tests/07_WebTables/237_webtable1.spec.ts
+++ tests/07_WebTables/237_webtable1.spec.ts
@@ -4,9 +4,16 @@ test('test the webtable1',async ({page})=>{
 
     await page.goto("https://awesomeqa.com/webtable1.html");
 
-    const rows= await page.locator('//table[@border="1"]/tbody/tr').count();
+    const rows=  page.locator('//table[@border="1"]/tbody/tr');
+    const rowscount= await rows.count();
     console.log(rows);
 
+    for (let i=0;i<=rowscount-1;i++)
+    {
+      const  colsdata= await rows.nth(i).locator('td').allInnerTexts();
+       console.log(`Row${i+1}:`,colsdata);
+
+    }
     await page.pause();
 
 });
